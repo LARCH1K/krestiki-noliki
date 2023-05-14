92bf2092578b6d93f0fc3fea364b356d25d0e2af
LARCHIK <ovchinnikov20035696@gmail.com>
2023-05-14 11:41:19 +0500

    Implement random computer move

--- src/larchik/krestiki_noliki/components/ComputerMove.java
+++ src/larchik/krestiki_noliki/components/ComputerMove.java
@@ -1,8 +1,20 @@
 package larchik.krestiki_noliki.components;
 
+import larchik.krestiki_noliki.model.Cell;
 import larchik.krestiki_noliki.model.GameTable;
+
+import java.util.Random;
 
 public class ComputerMove {
     public void make(final GameTable gameTable) {
+        while (true) {
+            int number_row = new Random().nextInt(3);
+            int number_col = new Random().nextInt(3);
+            final Cell randomCell = new Cell(number_row, number_col);
+            if (gameTable.isEmpty(randomCell)) {
+                gameTable.setSign(randomCell, 'O');
+                break;
+            }
+        }
     }
 }
