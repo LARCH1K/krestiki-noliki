072f936345e937cc9e869efdc1661a26e1637df1
LARCHIK <ovchinnikov20035696@gmail.com>
2023-05-15 10:30:51 +0500

    Implement UserMove component

--- src/larchik/krestiki_noliki/components/UserMove.java
+++ src/larchik/krestiki_noliki/components/UserMove.java
@@ -1,8 +1,49 @@
 package larchik.krestiki_noliki.components;
 
+import larchik.krestiki_noliki.model.Cell;
 import larchik.krestiki_noliki.model.GameTable;
 
+import java.util.Scanner;
+
 public class UserMove {
+
+    private final char[][] mappingTable = {
+            {'7', '8', '9'},
+            {'4', '5', '6'},
+            {'1', '2', '3'}
+    };
+
     public void make(final GameTable gameTable) {
+        while (true) {
+            final Cell cell = makeUserInput();
+            if (gameTable.isEmpty(cell)) {
+                gameTable.setSign(cell, 'X');
+                return;
+            } else {
+                System.out.println("Can't make a move, because the cell is not free! Try again!");
+            }
+        }
+
     }
+
+    private Cell makeUserInput() {
+        while (true) {
+            System.out.println("Please type number between 1 and 9:");
+            final String userInput = new Scanner(System.in).nextLine();
+            if (userInput.length() == 1) {
+                final char userInputChar = userInput.charAt(0);
+                if (userInputChar >= '1' && userInputChar <= '9') {
+                    for (int i = 0; i < 3; i++) {
+                        for (int j = 0; j < 3; j++) {
+                            if (mappingTable[i][j] == userInputChar) {
+                                return (new Cell(i, j));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
 }
